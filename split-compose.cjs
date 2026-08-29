3f6435cdbda0374b617118bd174fd04cc973542e
yahir140306 <isidrocej@gmail.com>
2026-08-29 13:54:00 -0600

    chore(android): remove temporary refactoring scripts

--- split-compose.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const srcPath = path.join(__dirname, 'app/src/main/java/com/example/rantu/Components/RoomDetailScreen.kt');
-let content = fs.readFileSync(srcPath, 'utf8');
-
-// I'll define an easier approach. I will just do it manually with a powerful prompt in a subagent?
-// No, I can do it right here.
-console.log("File length:", content.split('\n').length);
